71b3a3da2be3c1cdaf2ead34f033603c3dd5e619
Ademola Fadumo <demolafadumo@gmail.com>
2023-07-30 22:06:04 +0100

    feat: updating themes

--- lib/src/app/view/app.dart
+++ lib/src/app/view/app.dart
@@ -1,31 +1,17 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_bloc/src/core/constants/theme.dart';
 import 'package:todo_bloc/src/data/repositories/settings/settings_repository.dart';
 import 'package:todo_bloc/src/data/repositories/settings/settings_repository_impl.dart';
 import 'package:todo_bloc/src/l10n/l10n.dart';
 import 'package:todo_bloc/src/modules/landing_loading/view/landing_loading_view.dart';
 import 'package:todo_bloc/src/modules/settings/cubit/settings_cubit.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  // Used to select if we use the dark or light theme, start with system mode.
-  ThemeMode themeMode = ThemeMode.system;
-
-  // Opt in/out on Material-3
-  bool useMaterial3 = true;
-
-  @override
   Widget build(BuildContext context) {
-    const usedScheme = FlexScheme.mandyRed;
-
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider<SettingsRepository>(
@@ -38,28 +24,10 @@ class _AppState extends State<App> {
           builder: (context, state) {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
-              // Use the above dark or light theme based on active themeMode.
-              themeMode: themeMode,
-              theme: FlexThemeData.light(
-                scheme: usedScheme,
-                // Use very subtly themed app bar elevation in light mode.
-                appBarElevation: 0.5,
-                useMaterial3: useMaterial3,
-                // We use the nicer Material-3 Typography in both M2 and
-                // M3 mode.
-                typography:
-                    Typography.material2021(platform: defaultTargetPlatform),
-              ),
-              darkTheme: FlexThemeData.dark(
-                scheme: usedScheme,
-                // Use a bit more themed elevated app bar in dark mode.
-                appBarElevation: 2,
-                useMaterial3: useMaterial3,
-                // We use the nicer Material-3 Typography in both M2 and M3
-                // mode.
-                typography:
-                    Typography.material2021(platform: defaultTargetPlatform),
-              ),
+              theme: TodoBlocAppTheme.lightTheme,
+              darkTheme: TodoBlocAppTheme.darkTheme,
+              // TODO(demolaf): change to use system
+              // themeMode: ThemeMode.system,
               localizationsDelegates: AppLocalizations.localizationsDelegates,
               supportedLocales: AppLocalizations.supportedLocales,
               home: const LandingLoadingView(),
--- lib/src/core/constants/theme.dart
+++ lib/src/core/constants/theme.dart
@@ -1,26 +1,43 @@
-import 'dart:ui';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class TodoBlocAppTheme {
+  static final lightTheme = FlexThemeData.light(
+    useMaterial3: true,
+    colors: _flexScheme.light,
+    visualDensity: VisualDensity.standard,
+    fontFamily: 'Montserrat',
+    typography: Typography.material2021(platform: defaultTargetPlatform),
+  );
+
+  static final darkTheme = FlexThemeData.dark(
+    useMaterial3: true,
+    colors: _flexScheme.dark,
+    fontFamily: 'Montserrat',
+    visualDensity: VisualDensity.standard,
+    typography: Typography.material2021(platform: defaultTargetPlatform),
+  );
+
   // ignore: unused_field
-  static const FlexSchemeData _myFlexScheme = FlexSchemeData(
+  static const FlexSchemeData _flexScheme = FlexSchemeData(
     name: 'Midnight blue',
     description: 'Midnight blue theme, custom definition of all colors',
     light: FlexSchemeColor(
-      primary: Color(0xFF00296B),
-      primaryContainer: Color(0xFFA0C2ED),
-      secondary: Color(0xFFD26900),
-      secondaryContainer: Color(0xFFFFD270),
-      tertiary: Color(0xFF5C5C95),
-      tertiaryContainer: Color(0xFFC8DBF8),
+      primary: Color(0xFFE8A0BF),
+      primaryContainer: Color(0xFFE56CA0),
+      secondary: Color(0xFFBA90C6),
+      secondaryContainer: Color(0xFFAF60C6),
+      tertiary: Color(0xFFC0DBEA),
+      tertiaryContainer: Color(0xFF87C5E8),
     ),
     dark: FlexSchemeColor(
-      primary: Color(0xFFB1CFF5),
-      primaryContainer: Color(0xFF3873BA),
-      secondary: Color(0xFFFFD270),
-      secondaryContainer: Color(0xFFD26900),
-      tertiary: Color(0xFFC9CBFC),
-      tertiaryContainer: Color(0xFF535393),
+      primary: Color(0xFF150050),
+      primaryContainer: Color(0xFF3400C5),
+      secondary: Color(0xFF6F00C8),
+      secondaryContainer: Color(0xFF3F0071),
+      tertiary: Color(0xFFFB2576),
+      tertiaryContainer: Color(0xFFFF73A8),
     ),
   );
 }
