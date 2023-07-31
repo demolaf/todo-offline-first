e7160a9f912d5b29977eba747c52f6e0d62e00ce
Ademola Fadumo <demolafadumo@gmail.com>
2023-07-31 15:05:20 +0100

    feat: added local storage functionality with realm

--- lib/src/data/local_storage/local_storage.dart
+++ lib/src/data/local_storage/local_storage.dart
@@ -1,1 +1,24 @@
-abstract class LocalStorage {}
+import 'package:realm/realm.dart';
+
+abstract class LocalStorage {
+  Future<void> create<T>(T object);
+
+  Future<void> update<T>(T object);
+
+  Future<void> delete<T>(T object);
+
+  /// Realm Specific methods
+  T? findObjectById<T extends RealmObject>(ObjectId id);
+
+  RealmResults<T> read<T extends RealmObject>({required String query});
+
+  RealmResults<T> readAll<T extends RealmObject>();
+
+  Stream<RealmResultsChanges<T>> readAsStream<T extends RealmObject>({
+    required String query,
+  });
+
+  Stream<RealmResultsChanges<T>> readAllAsStream<T extends RealmObject>();
+
+  void dispose();
+}
