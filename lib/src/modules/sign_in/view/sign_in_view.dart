af0fa602246a624792a205b97faf11d0d48af07c
Ademola Fadumo <demolafadumo@gmail.com>
2023-08-01 22:44:34 +0100

    feat: sign in ui + cubit skeleton

--- lib/src/modules/sign_in/view/sign_in_view.dart
+++ lib/src/modules/sign_in/view/sign_in_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_bloc/src/data/repositories/auth/auth_repository.dart';
+import 'package:todo_bloc/src/modules/create_todo/bloc/create_todo_bloc.dart';
+import 'package:todo_bloc/src/modules/sign_in/cubit/sign_in_cubit.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
@@ -6,7 +10,12 @@ class SignInView extends StatelessWidget {
   static Route<SignInView> route() {
     return MaterialPageRoute(
       builder: (context) {
-        return const SignInView();
+        return BlocProvider(
+          create: (context) => SignInCubit(
+            authRepository: context.read<AuthRepository>(),
+          ),
+          child: const SignInView(),
+        );
       },
     );
   }
@@ -14,8 +23,61 @@ class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: GestureDetector(
+                onTap: () async {
+                  await context.read<SignInCubit>().signInUserWithGoogle();
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: BlocSelector<SignInCubit, SignInState, bool>(
+                    selector: (state) {
+                      if (state is SignInStateAuthenticating) {
+                        return state.processingState ==
+                            ProcessingState.processing;
+                      }
+                      return false;
+                    },
+                    builder: (context, isAuthenticating) {
+                      if (isAuthenticating) {
+                        return Center(
+                          child: SizedBox(
+                            width: 24,
+                            child: CircularProgressIndicator(
+                              color: Theme.of(context).colorScheme.onBackground,
+                              strokeWidth: 3,
+                            ),
+                          ),
+                        );
+                      }
+                      return Center(
+                        child: Text(
+                          'Login with Google',
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
